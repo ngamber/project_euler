5870047c015cf99b37af3c5d8c8b0214d850b2de
Nathan Gamber <ngamber@gmail.com>
2011-01-23 16:26:05 -0500

    add 09

---
+++ 9.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a^(2) + b^(2) = c^(2
+#Find the triplet for which a+b+c=1000
+
+def triplet_check(a,b,c)
+	if a**2+b**2 == c**2
+		return true
+	else
+		return false
+	end
+end
+
+
+(1..500).each do |k|
+	(2..k).each do |j|
+		(3..j).each do |i|
+			if i + j + k == 1000
+				if triplet_check(i,j,k)
+					puts i,j,k
+				end
+			end
+		end
+	end
+end
